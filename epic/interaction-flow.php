b3d52160fbaffecefb82f1c4c113d8bedbe37bf3
Joshua Rodriguez <jrodriguez332@cnm.edu>
2019-04-12 10:36:02 -0600

    More edits on the post/pre conditions and interaction flow.

--- epic/interaction-flow.php
+++ epic/interaction-flow.php
@@ -10,19 +10,19 @@
 			<p><b>Description:</b> Sarah wants to be able to add the songs that she likes to her playlist</p>
 			<p><b>Name of user & their role:</b> Jane; loves music and goes through soundcloud to find it.</p>
 			<p><b>Usage preconditions:</b> Has to have a soundcloud account in order to add to a playlist.</p>
-			<p><b>User postconditions:</b> Artist song is liked, reposted, and added to her
+			<p><b>User postconditions:</b> All of the artists songs she searched for can now be added to her
 				playlist for future listens.</p>
 		</div>
 		<div>
-			<h2>Interaction Flow</h2>
+			<h2>Interaction Flow></h2>
 			<ul>
 				<li>Sarah enters soundcloud.com into her web browser</li>
 				<li>It leads her to the soundcloud homepage.</li>
 				<li>She logs into her account. Which takes her to her main page.</li>
 				<li>On her main page she puts her cursor over the search bar.</li>
-				<li>She finds the artist she was looking for and is linked to their main page.</li>
+				<li>She types in the name of the artist and is linked to their main page.</li>
 				<li>She scrolls through all their songs until she finds the one she likes.</li>
-				<li>After clicking the song she it takes her to the songs page where it's streaming.</li>
+				<li>After clicking the song, it takes her to the songs page where it's streaming.</li>
 				<li>Under the song there is a button which can add it to her playlist of her choosing.</li>
 				<li>After adding the song to her playlist she clicks on her profile in the top right
 					which has an option to link her to her playlist.</li>
